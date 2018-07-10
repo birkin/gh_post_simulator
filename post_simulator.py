5473c5b25f119d222be53be78b304d5bdf9b1168
birkin <birkin.diana@gmail.com>
2018-07-10 15:15:11 -0400

    simplifies logging.

--- post_simulator.py
+++ post_simulator.py
@@ -4,21 +4,30 @@
     Assumes python3 env is activated. """
 
 import json, logging, logging.config, os, pathlib, pprint, sys
-
-cwd = os.getcwd()  # so this assumes the cron call has cd-ed into the project directory
-if cwd not in sys.path:
-    print( 'adding ```%s``` to sys.path' % cwd  )
-    sys.path.append( cwd )
-
 import requests
+
+# cwd = os.getcwd()  # so this assumes the cron call has cd-ed into the project directory
+# if cwd not in sys.path:
+#     print( 'adding ```%s``` to sys.path' % cwd  )
+#     sys.path.append( cwd )
+
+
+logging.basicConfig(
+    filename=os.environ['GH_SIM__LOG_PATH'],
+    level=logging.DEBUG,
+    format='[%(asctime)s] %(levelname)s [%(module)s-%(funcName)s()::%(lineno)d] %(message)s',
+    datefmt='%d/%b/%Y %H:%M:%S',
+    )
+logging.propagte = False
+log = logging.getLogger(__name__)
 
 
 URL = os.environ[ 'GH_SIM__BASICAUTH_LISTENER_URL' ]
 PAYLOAD = os.environ[ 'GH_SIM__PAYLOAD' ]
-LOG_CONFIG_DCT = os.environ[ 'GH_SIM__LOG_CONFIG_DCT' ]
+# LOG_CONFIG_DCT = os.environ[ 'GH_SIM__LOG_CONFIG_DCT' ]
 
 
-log = logging.getLogger( 'gh_post_sim_logger' )
+# log = logging.getLogger( 'gh_post_sim_logger' )
 config_dct = json.loads( LOG_CONFIG_DCT )
 logging.config.dictConfig( config_dct )
 log.debug( 'logging ready' )
