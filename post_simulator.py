34bb1829f90b743676fb7642f17d3874dc1777e7
birkin <birkin.diana@gmail.com>
2018-07-10 15:16:16 -0400

    simplifies logging.

--- post_simulator.py
+++ post_simulator.py
@@ -20,17 +20,12 @@ logging.basicConfig(
     )
 logging.propagte = False
 log = logging.getLogger(__name__)
+log.debug( 'logging ready' )
 
 
 URL = os.environ[ 'GH_SIM__BASICAUTH_LISTENER_URL' ]
 PAYLOAD = os.environ[ 'GH_SIM__PAYLOAD' ]
-# LOG_CONFIG_DCT = os.environ[ 'GH_SIM__LOG_CONFIG_DCT' ]
 
-
-# log = logging.getLogger( 'gh_post_sim_logger' )
-config_dct = json.loads( LOG_CONFIG_DCT )
-logging.config.dictConfig( config_dct )
-log.debug( 'logging ready' )
 
 
 
