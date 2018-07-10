7716e9ee5bf99b90e34171e7114f2d3ca245f4e5
birkin <birkin.diana@gmail.com>
2018-07-10 15:07:04 -0400

    updates envar-prefix.

--- post_simulator.py
+++ post_simulator.py
@@ -4,12 +4,18 @@
     Assumes python3 env is activated. """
 
 import json, logging, logging.config, os, pathlib, pprint, sys
+
+cwd = os.getcwd()  # so this assumes the cron call has cd-ed into the project directory
+if cwd not in sys.path:
+    print( 'adding ```%s``` to sys.path' % cwd  )
+    sys.path.append( cwd )
+
 import requests
 
 
-URL = os.environ[ 'POST_SIMULATOR__BASICAUTH_LISTENER_URL' ]
-PAYLOAD = os.environ[ 'POST_SIMULATOR__PAYLOAD' ]
-LOG_CONFIG_DCT = os.environ[ 'POST_SIMULATOR__LOG_CONFIG_DCT' ]
+URL = os.environ[ 'GH_SIM__BASICAUTH_LISTENER_URL' ]
+PAYLOAD = os.environ[ 'GH_SIM__PAYLOAD' ]
+LOG_CONFIG_DCT = os.environ[ 'GH_SIM__LOG_CONFIG_DCT' ]
 
 
 log = logging.getLogger( 'gh_post_sim_logger' )
