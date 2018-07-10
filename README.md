fce77481a20c9bc6332a5b3b46a1da89344006ee
birkin <birkin.diana@gmail.com>
2018-07-10 15:52:06 -0400

    adds info re settings file.

--- README.md
+++ README.md
@@ -7,6 +7,14 @@ It's nice to be able to test these without actually having to change real data, 
 
 This code does that.
 
+Usage...
+
+- create a 'stuff' directory
+- create a virtual-environment env directory in the stuff directory
+- git-clone this project into the stuff directory
+- put the settings file in the stuff directory (or elsewhere)
+- add a line to the bottom of the `env/bin/activate` file pointing to the settings file
+
 Run like this:
 
         cd /path/to/project/; source ../env/bin/activate; ../env/bin/python3 ./post_simulator.py
--- sample_env_settings.sh
+++ sample_env_settings.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 
-## Place this file somewhere out of the project path,
-## ...update these settings, and call it via `../env/bin/activate`.
-## See the README for more info.
+## Settings instructions...
+## - place this file somewhere out of the project path
+## - edit the virtual-environment's `env/bin/activate` file by adding, at the bottom, the line:
+##   `source "/path/to/this_settings_file.sh"` (no backticks)
+## - update these settings
+## - see the README for more info
 
 export GH_SIM__LOG_PATH="/path/to/gh_simulator.log"
 
